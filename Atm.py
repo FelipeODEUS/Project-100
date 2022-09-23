7cc853ce6ee2203b2d8119b438a23784ed6c6826
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-09-23 15:02:06 -0300

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,16 @@
+from cmath import pi
+
+
+class Atm:
+    def __init__(self, cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+class BalanceEnquiry:
+    def check_balance(self):
+        print("Seu balanço é 50000")
+
+class CashWithdrawl:
+    def cash_withdrawl(self):
+        print("seu dinheiro foi retirado") 
+  
